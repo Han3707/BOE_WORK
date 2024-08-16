25af25be44c33409d8c39ac45010902652f2a131
Han3707 <105863807+Han3707@users.noreply.github.com>
2024-08-16 10:50:20 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 1212 ms, Memory: 82248 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -1,51 +1,56 @@
+
 import java.io.*;
 import java.util.*;
 
 public class Main {
-	static int n;
-	static int a,b;
-	static boolean[] visit;
-	static int[] p_node;
-	static ArrayList<Integer>[] child;
-
-	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = null;
-		n = Integer.parseInt(br.readLine());
+		int n = Integer.parseInt(br.readLine());
+		//각 노드의 부모 노드를 저장하는 배열
+		int[] parent = new int[n+1];
+		//연결 리스트 배열
+		ArrayList<Integer>[] adj = new ArrayList[n+1];
 		
-		visit = new boolean[n+1];
-		p_node = new int[n+1];
-		child = new ArrayList[n+1];
-		for(int i =0; i < n+1 ; i++) {
-			child[i] = new ArrayList<>();
+		for (int i = 1; i <= n; i++) {
+			adj[i] = new ArrayList<>();
 		}
-		
-		
-		for(int i =1; i < n; i++) {
+		// 방문 체크 배열
+		boolean[] visited = new boolean[n+1];
+		StringTokenizer st;
+		for (int i = 1; i < n; i++) {
 			st = new StringTokenizer(br.readLine());
-			a = Integer.parseInt(st.nextToken());
-			b = Integer.parseInt(st.nextToken());
-			child[a].add(b);
-			child[b].add(a);
-		}
-		dfs(1);
-		for(int i = 2; i < n+1; i++) {
-			System.out.println(p_node[i]);
-		}
-	}
-	
-	static void dfs(int node) {
-		
-		visit[node] = true;
-		for(int i = 0; i< child[node].size(); i++) {
-			if(!visit[child[node].get(i)]) {
-				p_node[child[node].get(i)] = node;
-				dfs(child[node].get(i));
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			adj[a].add(b);
+			adj[b].add(a);
 		}
 		
-	
+		//BFS를 위한 큐
+		Queue<Integer> q = new LinkedList<>();
+		q.add(1);
+		visited[1] = true;
+		
+		//BFS
+		while(!q.isEmpty()) {
+			int cur = q.poll();
+			//현재 노드에 연결된 다음 노드들을 탐색
+			for (int next : adj[cur]) {
+				//방문했던 노드라면 건너뛴다
+				if (visited[next]) {
+					continue;
+				}
+				//다음 노드를 방문처리하고 큐에 넣는다
+				visited[next] = true;
+				q.add(next);
+				//다음 노드의 부모가 현재 노드임을 저장한다.
+				parent[next] = cur;
+			}
+		}
+		for (int i = 2; i <= n; i++) {
+			System.out.println(parent[i]);
+		}
+		
+		
 	}
 
-}
 }
