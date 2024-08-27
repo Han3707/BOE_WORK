e858a0f10422a22d89d9cd8b0dc35d2a427ec57c
Han3707 <105863807+Han3707@users.noreply.github.com>
2024-08-27 12:20:32 +0900

    [Silver III] Title: N과 M (7), Time: 544 ms, Memory: 123776 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int n, m;
+    static int[] arr;
+    static StringBuilder sb = new StringBuilder(); // StringBuilder 사용
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+        nm5(0, new int[m]);
+
+        System.out.print(sb.toString()); // 한번에 출력
+    }
+
+    private static void nm5(int cnt, int[] selected) {
+        if (cnt == m) {
+            for (int i = 0; i < selected.length; i++) {
+                sb.append(selected[i]).append(" "); // StringBuilder에 추가
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            selected[cnt] = arr[i];
+            nm5(cnt + 1, selected);
+        }
+    }
+}
