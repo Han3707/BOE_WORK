1978ac8b5cdb639f6fde3f8b9f63c80f7775a087
Han3707 <105863807+Han3707@users.noreply.github.com>
2024-08-30 15:10:23 +0900

    [Silver II] Title: N과 M (9), Time: 908 ms, Memory: 36152 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int n, m;
+    static int[] num, ans;
+    static boolean[] visited;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken()); // 전역 변수 n 할당
+        m = Integer.parseInt(st.nextToken()); // 전역 변수 m 할당
+
+        num = new int[n];
+        ans = new int[m];
+        visited = new boolean[n];
+
+        st = new StringTokenizer(br.readLine()); // 새로운 라인에서 숫자들 읽기
+
+        for (int i = 0; i < n; i++) {
+            num[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(num); // 숫자를 오름차순으로 정렬
+        dfs(0);
+    }
+
+    static void dfs(int depth) {
+        if (depth == m) {
+            for (int i = 0; i < m; i++) {
+                System.out.print(ans[i] + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+        int last = 0; // 마지막으로 사용된 숫자 저장용 변수
+        for (int i = 0; i < n; i++) {
+            if (!visited[i] && last != num[i]) {
+                visited[i] = true;
+                ans[depth] = num[i];
+                last = num[i];
+                dfs(depth + 1);
+                visited[i] = false;
+            }
+        }
+    }
+}
