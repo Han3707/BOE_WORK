9e96f9ad12b32c3f0d594ca54b2ce3e78f6dde46
Han3707 <105863807+Han3707@users.noreply.github.com>
2024-09-07 23:59:55 +0900

    [Silver IV] Title: 설탕 배달, Time: 104 ms, Memory: 14340 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,26 +1,32 @@
-import java.io.InputStreamReader;
-import java.io.BufferedReader;
-import java.io.IOException;
- 
+import java.io.*;
+
 public class Main {
- 
-	public static void main(String[] args) throws IOException {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		
- 
-		if (N == 4 || N == 7) {
-			System.out.println(-1);
-		}
-		else if (N % 5 == 0) {
-			System.out.println(N / 5);
-		}
-		else if (N % 5 == 1 || N % 5 == 3) {
-			System.out.println((N / 5) + 1);
-		}
-		else if (N % 5 == 2 || N % 5 == 4) {
-			System.out.println((N / 5) + 2);
-		}
-	}
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine()); //배달해야하는 무게가 총 몇 kg
+        br.close();
+
+        int basket =  0; //총 몇개의 봉지가 들어가는지
+
+        while(N>0){
+            if(N % 5  == 0){ //5로 나누어지면
+                basket += N/5;
+                break;
+            }else { //만약 아니라면 3KG이거나 봉지에 담지 나눠담지 못하는 것.
+                N -= 3;
+                basket++;
+            }
+            if(N < 0){ //차피 나눠지지 않는다면 else문에서 -3을 했기 때문에 0보다 작을 것임...
+                basket = -1;
+            }
+        }
+        bw.write(basket + "");
+        bw.flush();
+        bw.close();
+
+    }
 }
