bb28f98ee1f44b190ba3ee07dac8e6d1c9081eab
Han3707 <105863807+Han3707@users.noreply.github.com>
2024-09-12 15:23:47 +0900

    [Bronze II] Title: Hashing, Time: 104 ms, Memory: 14168 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -2,22 +2,24 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
 
         int n = Integer.parseInt(br.readLine());
         String str = br.readLine();
 
-        int[] arr = new int[n];
+        long[] arr = new long[n];
+        long ans = 0;
+        long pow = 1;
+        long mod = 1234567891;
 
-        int ans =0;
-        for(int i=0; i<n; i++){
-            arr[i] = (str.charAt(i)-'0' -48);
+        for (int i = 0; i < n; i++) {
+            arr[i] = (str.charAt(i) - 'a' + 1); // 알파벳을 숫자로 변환
 
-            ans += arr[i] * Math.pow(31,i);
+            ans = (ans + arr[i] * pow) % mod;
+            pow = (pow * 31) % mod; // 제곱 값을 매번 계산 (값이 커지면 오버플로우 나기 때문에 미리)
         }
-        System.out.println(ans);
 
+        System.out.println(ans);
     }
 }
