937c11151b4134ddae268c89dcb0e0aa5d8a50b9
Han3707 <105863807+Han3707@users.noreply.github.com>
2024-09-21 18:21:37 +0900

    [Gold IV] Title: 좋다, Time: 152 ms, Memory: 14524 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.java
+++ 백준/Gold/1253. 좋다/좋다.java
@@ -1,15 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt(); // 배열의 크기
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine()); // 배열의 크기
         int[] arr = new int[n];
-
+        StringTokenizer st;
         // 배열 입력
+        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
         // 배열 오름차순 정렬
@@ -18,7 +22,7 @@ public class Main {
 
         // 각 원소를 타겟으로 설정하여 확인
         for (int i = 0; i < n; i++) {
-            if (isGood(arr, i)) {
+            if (Check(arr, i)) {
                 count++;
             }
         }
@@ -27,7 +31,7 @@ public class Main {
     }
 
     // 현재 인덱스의 원소가 두 수의 합으로 표현될 수 있는지 확인
-    private static boolean isGood(int[] arr, int index) {
+    private static boolean Check(int[] arr, int index) {
         int target = arr[index];
         int left = 0;
         int right = arr.length - 1;
@@ -46,9 +50,10 @@ public class Main {
 
             if (sum == target) {
                 return true; // 두 수의 합이 타겟과 같으면 true 반환
-            } else if (sum < target) {
+                // left( index = 0  작은 수부터 시작 ) right( index = arr.length-1 큰 수부터 시작 )
+            } else if (sum < target) { // 합이 타겟보다 작으면 값을 올려야하므로 left++ 
                 left++;
-            } else {
+            } else { // 합이 타겟보다 크면 값이 큰 것이므로 right -- 
                 right--;
             }
         }
