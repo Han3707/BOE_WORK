e3c2b7433929fa3f1f853d6ed1252aaab4558faa
Han3707 <105863807+Han3707@users.noreply.github.com>
2024-10-15 22:03:55 +0900

    [Gold III] Title: 같이 눈사람 만들래?, Time: 232 ms, Memory: 14568 KB -BaekjoonHub

--- 백준/Gold/20366. 같이 눈사람 만들래？/같이 눈사람 만들래？.java
+++ 백준/Gold/20366. 같이 눈사람 만들래？/같이 눈사람 만들래？.java
@@ -3,14 +3,14 @@ import java.util.*;
 
 
 public class Main {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
         int[] arr = new int[N];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i=0; i<N; i++){
+        for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
@@ -18,35 +18,36 @@ public class Main {
 
         int snow_King = 0;
         int res = Integer.MAX_VALUE;
-        for(int i=0; i<N; i++){
-            for(int j=i+1; j<N; j++){
-                snow_King = arr[i]+arr[j];
-                int left =0;
-                int right = N-1;
+        cute:
+        for (int i = 0; i < N; i++) {
+            for (int j = i + 1; j < N; j++) {
+                snow_King = arr[i] + arr[j];
+                int left = 0;
+                int right = N - 1;
 
-                while(left < right){
+                while (left < right) {
 
-                    if(left == i || left == j){
+                    if (left == i || left == j) {
                         left++;
                         continue;
                     }
 
-                    if(right ==i || right ==j){
+                    if (right == i || right == j) {
                         right--;
                         continue;
                     }
 
                     int snow_man = arr[left] + arr[right];
 
-                    res = Math.min(res,Math.abs(snow_King-snow_man));
+                    res = Math.min(res, Math.abs(snow_King - snow_man));
 
-                    if(snow_King > snow_man){
+                    if (snow_King > snow_man) {
                         left++;
-                    }else if(snow_King < snow_man){
+                    } else if (snow_King < snow_man) {
                         right--;
                     }else{
-                        res = 0;
-                        break;
+                        res =0;
+                        break cute;
                     }
                 }
             }
