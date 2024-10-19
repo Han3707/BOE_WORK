0f94753eb176fb820465de57fe42d694b8132428
Han3707 <105863807+Han3707@users.noreply.github.com>
2024-10-19 19:29:54 +0900

    [Silver I] Title: 숨바꼭질, Time: 148 ms, Memory: 22972 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -1,79 +1,40 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-
-// 수빈이는 동생과 숨바꼭질을 하고 있다. 수빈이는 현재 점 N에 있고, 동생은 점 K에 있다.
-// 수빈이는 걷거나 순간이동을 할 수 있다.
-// 만약 수빈이의 위치가 X일 때 걷는다면 1초 후에 X-1 또는 X+1로 이동하게 된다.
-// 순간이동을 하는 경우에는 1초 후에 2*X의 위치로 이동하게 된다.
-// 수빈이와 동생의 위치가 주어졌을 때,
-// 수빈이가 동생을 찾을 수 있는 가장 빠른 시간이 몇 초 후인지 구하는 프로그램을 작성하시오.
-
+import java.io.*;
+import java.util.*;
 
 public class Main {
+  static int n, k;
+  static boolean[] visited ;
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
 
-    static class Point {
-        int val;
-        int cnt;
+    n = Integer.parseInt(st.nextToken());
+    k = Integer.parseInt(st.nextToken());
+    int time = 0;
+    int res = BFS(n);
+    System.out.println(res);
+  }
 
-        public Point(int val, int cnt) {
-            this.val = val;
-            this.cnt = cnt;
-        }
+  static int BFS(int start) {
+    Queue<int[]> q = new ArrayDeque<>();
+    visited = new boolean[1000001];
+    q.add(new int[]{start,0});
+
+    while(!q.isEmpty()){
+      int[] cur = q.poll();
+
+      int cur_pos = cur[0];
+      int cur_time = cur[1];
+
+      if(cur_pos == k) return cur_time;
+
+      if(cur_pos >= 0 && cur_pos < 100001 && !visited[cur_pos]){
+        visited[cur_pos] = true;
+        q.add(new int[]{cur_pos*2,cur_time+1});
+        q.add(new int[]{cur_pos+1,cur_time+1});
+        q.add(new int[]{cur_pos-1,cur_time+1});
+      }
     }
-    static int min = Integer.MAX_VALUE;
-    static boolean[] visited = new boolean[100001];
-    public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-
-        String[] s = reader.readLine().split(" ");
-        int N = Integer.parseInt(s[0]);
-        int K = Integer.parseInt(s[1]);
-        Point start = new Point(N, 0);
-        Queue<Point> queue = new LinkedList<>();
-        queue.add(start);
-
-        while(!queue.isEmpty()) {
-            Point poll = queue.poll();
-            int curr = poll.val;
-            int cnt = poll.cnt;
-
-            if(curr < 0 && curr > 100001) {
-                continue;
-            }
-            if(visited[curr]) {
-                continue;
-            }
-
-            visited[curr] = true;
-
-            if(curr == K && cnt < min) {
-                min = cnt;
-                System.out.println(min);
-                return;
-            }
-
-            if(curr * 2 >= 0 && curr * 2 < 100001 ) {
-                if(!visited[curr*2]) {
-                    Point mul = new Point(curr * 2, poll.cnt + 1);
-                    queue.add(mul);
-                }
-            }
-            if(curr + 1 < 100001) {
-                if(!visited[curr+1]) {
-                    Point add = new Point(curr + 1, poll.cnt + 1);
-                    queue.add(add);
-                }
-            }
-            if(curr - 1 >= 0 && curr - 1 < 100001) {
-                if(!visited[curr-1]) {
-                    Point sub = new Point(curr - 1, poll.cnt + 1);
-                    queue.add(sub);
-                }
-            }
-
-        }
-    }
+    return -1;
+  }
 }
