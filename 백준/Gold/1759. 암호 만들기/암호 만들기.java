58a75c73e014cd8c6c70335d45a4ae8192f25d44
Han3707 <105863807+Han3707@users.noreply.github.com>
2024-10-28 22:27:40 +0900

    [Gold V] Title: 암호 만들기, Time: 124 ms, Memory: 14820 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.java
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -8,7 +8,7 @@ public class Main {
     static int n, m;
     static char[] arr;
     static List<String> result;
-    static final Set<Character> vowels = Set.of('a', 'e', 'i', 'o', 'u');
+    static Set<Character> vowels = Set.of('a', 'e', 'i', 'o', 'u');
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -23,7 +23,7 @@ public class Main {
         for (int i = 0; i < m; i++) {
             arr[i] = st.nextToken().charAt(0);
         }
-        Arrays.sort(arr); // 정렬하여 사전 순서 유지
+        Arrays.sort(arr); // 사전 순서로 정렬
         result = new ArrayList<>();
         combination(0, 0, 0, 0);
 
@@ -32,23 +32,24 @@ public class Main {
         }
     }
 
-    static void combination(int idx, int depth, int vowelCount, int consonantCount) {
+    static void combination(int idx, int depth, int Acnt, int Bcnt) {
         if (depth == n) {
-            if (vowelCount >= 1 && consonantCount >= 2) {
+            if (Acnt >= 1 && Bcnt >= 2) {
                 result.add(sb.toString()); // 조건을 만족할 때만 리스트에 추가
             }
             return;
         }
 
         for (int i = idx; i < m; i++) {
-            char currentChar = arr[i];
-            sb.append(currentChar);
+            char temp = arr[i];
+            sb.append(temp);
 
             // 모음 또는 자음인지 확인하여 카운트 증가
-            if (vowels.contains(currentChar)) {
-                combination(i + 1, depth + 1, vowelCount + 1, consonantCount);
+            // 모음 = Acnt , 자음 = Bcnt
+            if (vowels.contains(temp)) {
+                combination(i + 1, depth + 1, Acnt + 1, Bcnt);
             } else {
-                combination(i + 1, depth + 1, vowelCount, consonantCount + 1);
+                combination(i + 1, depth + 1, Acnt, Bcnt + 1);
             }
 
             sb.deleteCharAt(sb.length() - 1); // 백트래킹
