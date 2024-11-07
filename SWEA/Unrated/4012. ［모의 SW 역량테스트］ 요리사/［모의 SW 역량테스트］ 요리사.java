02dd964a0902667158e42ec40a67249ef6b3c3dc
Han3707 <105863807+Han3707@users.noreply.github.com>
2024-11-07 15:04:34 +0900

    [Unrated] Title: [모의 SW 역량테스트] 요리사, Time: 246 ms, Memory: 23,048 KB -BaekjoonHub

--- SWEA/Unrated/4012. ［모의 SW 역량테스트］ 요리사/［모의 SW 역량테스트］ 요리사.java
+++ SWEA/Unrated/4012. ［모의 SW 역량테스트］ 요리사/［모의 SW 역량테스트］ 요리사.java
@@ -5,7 +5,7 @@ public class Solution {
     static int t, n;
     static int[][] map;
     static int minDiff;
-
+    static boolean[] visit;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         t = Integer.parseInt(br.readLine());
@@ -15,6 +15,7 @@ public class Solution {
             n = Integer.parseInt(br.readLine());
             map = new int[n][n];
             minDiff = Integer.MAX_VALUE;
+            visit = new boolean[n];
 
             // 시너지 맵 입력
             for (int i = 0; i < n; i++) {
@@ -24,46 +25,42 @@ public class Solution {
                 }
             }
 
+
+
             // 조합 생성 및 최소 차이 계산
-            generateGroups(new ArrayList<>(), 0, n / 2);
+            combination(0, 0,visit);
             System.out.println("#" + tc + " " + minDiff);
         }
     }
 
-    // group1을 선택해두고 나머지를 group2로 계산
-    static void generateGroups(List<Integer> group1, int start, int r) {
-        if (group1.size() == r) {
-            calculateMinDifference(group1);
+    static void combination(int start,int depth,boolean[] visit){
+
+        if(depth == n/2){
+            int sum1 =0;
+            int sum2 =0;
+
+            for(int i=0; i<n; i++){
+                for(int j=i+1; j<n; j++){
+                    if(visit[i] && visit[j]){
+                        sum1 += map[i][j] + map[j][i];
+                    }else if(!visit[i] && !visit[j]){
+                        sum2 += map[i][j] + map[j][i];
+                    }
+                }
+            }
+            int res = Math.abs(sum1-sum2);
+            minDiff = Math.min(minDiff,res);
             return;
         }
 
-        for (int i = start; i < n; i++) {
-            group1.add(i);
-            generateGroups(group1, i + 1, r);
-            group1.remove(group1.size() - 1);
-        }
-    }
-
-    // 두 그룹의 시너지 합 차이 계산
-    static void calculateMinDifference(List<Integer> group1) {
-        List<Integer> group2 = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            if (!group1.contains(i)) group2.add(i);
+        for(int i=start; i<n; i++){
+            visit[i] = true;
+            combination(i+1,depth+1,visit);
+            visit[i] = false;
         }
 
-        int synergy1 = calculateSynergy(group1);
-        int synergy2 = calculateSynergy(group2);
-        minDiff = Math.min(minDiff, Math.abs(synergy1 - synergy2));
     }
 
-    // 주어진 그룹의 시너지 합 계산
-    static int calculateSynergy(List<Integer> group) {
-        int sum = 0;
-        for (int i = 0; i < group.size(); i++) {
-            for (int j = i + 1; j < group.size(); j++) {
-                sum += map[group.get(i)][group.get(j)] + map[group.get(j)][group.get(i)];
-            }
-        }
-        return sum;
-    }
+
+
 }
