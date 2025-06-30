c69f7f40382c878f1a014b22b6abfc4979a96d82
HanMin Kim <105863807+Han3707@users.noreply.github.com>
2025-06-30 21:54:37 +0900

    [level 2] Title: 타겟 넘버, Time: 4.64 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,26 +1,6 @@
 class Solution {
-    
-    private int answer = 0;
-    
     public int solution(int[] numbers, int target) {
-        
-        dfs(numbers, target, 0, 0);
-        
+        int answer = 0;
         return answer;
-    }
-    
-    private void dfs(int[] numbers, int target, int index, int curSum){
-        
-        if(index == numbers.length){
-            //현재까지의 합이 타겟 넘버와 같으면
-            if(curSum == target){
-                answer++;
-            }
-            return;
-        }
-        // + 인경우
-        dfs(numbers,target,index +1, curSum + numbers[index]);
-        // - 인경우
-        dfs(numbers,target,index +1, curSum - numbers[index]);
     }
 }
