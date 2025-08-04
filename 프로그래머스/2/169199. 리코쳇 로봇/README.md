8fc6fb250e913ea25eea784ece14af8df2ea707b
HanMin Kim <105863807+Han3707@users.noreply.github.com>
2025-08-04 14:48:59 +0900

    [level 2] Title: 리코쳇 로봇, Time: 4.15 ms, Memory: 79.8 MB -BaekjoonHub

--- 프로그래머스/2/169199. 리코쳇 로봇/README.md
+++ 프로그래머스/2/169199. 리코쳇 로봇/README.md
@@ -1,10 +1,10 @@
 # [level 2] 리코쳇 로봇 - 169199 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/169199) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/169199?language=java) 
 
 ### 성능 요약
 
-메모리: 71.1 MB, 시간: 10.25 ms
+메모리: 79.8 MB, 시간: 4.15 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 08월 04일 14:26:10
+2025년 08월 04일 14:48:53
 
 ### 문제 설명
 
--- 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.java
+++ 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.java
@@ -2,85 +2,83 @@ import java.util.*;
 
 class Solution {
 
-    private int[][] grid;
-    private int n, m;
-
+    private int[][] map;
+    private int n,m;
+    
     public int solution(String[] board) {
         n = board.length;
         m = board[0].length();
-
+        
+        map = new int[n][m];
+        
         int[] start = new int[2];
-        int[] goal = new int[2];
-
-        // 1. 보드를 숫자로 변환하고 시작/도착 위치 찾기
-        parseBoard(board, start, goal);
-
-        // 2. BFS 탐색
-        return bfs(start, goal);
-    }
-
-    private void parseBoard(String[] board, int[] start, int[] goal) {
-        grid = new int[n][m];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                char c = board[i].charAt(j);
-                if (c == 'D') {
-                    grid[i][j] = 1; // 장애물
-                } else {
-                    if (c == 'R') {
-                        start[0] = i;
-                        start[1] = j;
-                    } else if (c == 'G') {
-                        goal[0] = i;
-                        goal[1] = j;
-                    }
-                    grid[i][j] = 0; // 빈 공간 (로봇, 목표 포함)
+        int[] end = new int[2];
+        
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(board[i].charAt(j) == 'D'){
+                    map[i][j] = 1;
+                }else if(board[i].charAt(j) == 'R'){
+                    map[i][j] = 0;
+                    start[0] = i;
+                    start[1] = j;
+                }else if(board[i].charAt(j) == 'G'){
+                    map[i][j] = 0;
+                    end[0] = i;
+                    end[1] = j;
+                }else{
+                    map[i][j] = 0;
                 }
             }
         }
+        
+        
+        return bfs(start,end);
     }
-
-    private int bfs(int[] start, int[] goal) {
-        Deque<int[]> queue = new ArrayDeque<>();
+    
+    private int bfs(int[] start,int[] end){
+        Deque<int[]> dq = new ArrayDeque<>();
         boolean[][] visited = new boolean[n][m];
-
-        queue.add(new int[]{start[0], start[1], 0}); // {행, 열, 이동횟수}
-        visited[start[0]][start[1]] = true;
-
-        int[] dr = {-1, 1, 0, 0}; // 상, 하, 좌, 우
-        int[] dc = {0, 0, -1, 1};
-
-        while (!queue.isEmpty()) {
-            int[] current = queue.poll();
-            int r = current[0];
-            int c = current[1];
-            int moves = current[2];
-
-            if (r == goal[0] && c == goal[1]) {
-                return moves;
+        
+        dq.add(new int[]{start[0],start[1],0});
+        
+        int[] dx = {1,0,-1,0};
+        int[] dy = {0,1,0,-1};
+        
+        while(!dq.isEmpty()){
+            int[] cur = dq.poll();
+            int x = cur[0];
+            int y = cur[1];
+            int move = cur[2];
+            
+            if(x == end[0] && y == end[1]){
+                return move;
             }
-
-            for (int dir = 0; dir < 4; dir++) {
-                int nr = r;
-                int nc = c;
-
-                while (true) {
-                    int nextR = nr + dr[dir];
-                    int nextC = nc + dc[dir];
-
-                    if (nextR < 0 || nextR >= n || nextC < 0 || nextC >= m || grid[nextR][nextC] == 1) {
+            
+            for(int i=0; i<4; i++){
+                int nx = x;
+                int ny = y;
+                
+                while(true){
+                    int cx = nx + dx[i];
+                    int cy = ny + dy[i];
+                    
+                    if(cx < 0 || cy < 0 || cx >= n || cy >= m || map[cx][cy] ==1){
                         break;
                     }
-                    nr = nextR;
-                    nc = nextC;
+                    
+                    nx = cx;
+                    ny = cy;
                 }
-
-                if (!visited[nr][nc]) {
-                    visited[nr][nc] = true;
-                    queue.add(new int[]{nr, nc, moves + 1});
+                
+                if(!visited[nx][ny]){
+                    visited[nx][ny] = true;
+                    dq.add(new int[]{nx,ny,move+1});
                 }
             }
         }
+        
         return -1;
     }
+
 }
