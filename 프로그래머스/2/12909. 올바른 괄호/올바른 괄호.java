18c69f0b499a697902fbdf201ebaeeedce53ab2a
HanMin Kim <105863807+Han3707@users.noreply.github.com>
2025-08-24 01:33:56 +0900

    [level 2] Title: 올바른 괄호, Time: 6.90 ms, Memory: 54.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    boolean solution(String s) {
+    int cnt = 0;
+    for(char c : s.toCharArray()) {
+        if(c == '(') cnt++;
+        else {
+            cnt--;
+            if(cnt < 0) return false;
+        }
+    }
+    return cnt == 0;
+  }
+}
