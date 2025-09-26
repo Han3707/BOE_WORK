d6afef638584e50b71f48e1ee13b69b017bfd373
HanMin Kim <105863807+Han3707@users.noreply.github.com>
2025-09-26 20:44:48 +0900

    [level 1] Title: 신규 아이디 추천, Time: 17.98 ms, Memory: 93.7 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -2,30 +2,33 @@ class Solution {
     public String solution(String new_id) {
         String answer = "";
         
-        // 대문자 -> 소문자
-        String s = new_id.toLowerCase();
+        // 1. 소문자 치환
+        new_id = new_id.toLowerCase();
         
-        // 2. 알파벳 소문자, 숫자 , - , _ , . 제외한 문자 제거
-        s = s.replaceAll("[^a-z0-9._-]","");
+        //2. 소문자,숫자,-,_,. 를 제외한 문자 제거
+        new_id = new_id.replaceAll("[^a-z0-9-_.]","");
         
-               
-        // 3  . 이 연속되면 . 하나로 변환
-        s = s.replaceAll("\\.+",".");
+        //3. .이 두 번이상 연속된 부분 . 하나로
+        new_id = new_id.replaceAll("\\.+",".");
         
-        // 4.   "." 이 처음이나 마지막에 위치한다면 제거
-        s = s.replaceAll("^\\.|\\.$","");
+        //4.  .이 처음이나 끝에 위치하면 제거
+        new_id = new_id.replaceAll("^\\.|\\.$","");
         
-        // 5. 빈문자열이면 a
-        if(s.isEmpty()) s = "a";
+        //5. 빈 문자열이면 a 대입
+        if(new_id.isEmpty()) new_id = "a";
         
-        //6. 문자길이가 15자 이상이면 자르고 끝에 마침표면 제거
-        if(s.length() > 15) s = s.substring(0,15);
-        s = s.replaceAll("\\.$","");
-        
-        //7. 길이가 2 이하라면 마지막 문자를 길이가 3이 될때까지 추가
-        while(s.length() <= 2){
-            s += s.charAt(s.length() - 1);
+        //6. 길이가 16자 이상이면 처음부터 15개까지만 출력 그리고 마침표가 끝에 위치하면 마침표 제거
+        if(new_id.length() > 15){
+            new_id = new_id.substring(0,15);
+            new_id = new_id.replaceAll("\\.$","");
         }
-        return s;
+        //7. new_id 의 길이가 2자 이하면 new_id 의 마지막 문자를 3일 될때까지 반복
+        while(new_id.length() < 3){
+            new_id += new_id.charAt(new_id.length()-1);
+        }
+        
+        answer = new_id;
+        
+        return answer;
     }
 }
